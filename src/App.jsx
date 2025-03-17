12d4dceccdc158531059577883b662fd7d7f5f7a
Victor Lorencio <victorlorencio@gmail.com>
2025-03-17 09:43:34 +0100

    Update file App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,38 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import HomePage from './pages/HomePage';
-import WorkoutsPage from './pages/WorkoutsPage';
-import CsgPage from './pages/CsgPage';
-import PolygonTop from './components/PolygonTop';
-import PolygonBottom from './components/PolygonBottom';
-import NavBar from './components/Navbar';
-import FooterSection from './components/FooterSection';
-import LoginPage from './pages/LoginPage';
-import CreateAccountPage from './pages/CreateAccountPage';
-import PerfilPage from './pages/PerfilPage';
-import ProtectedRoute from './utils/ProtectedRoute';
+import HomePage from "./pages/HomePage";
+import WorkoutsPage from "./pages/WorkoutsPage";
+import CsgPage from "./pages/CsgPage";
+import NavBar from "./components/Navbar";
+import FooterSection from "./components/FooterSection";
+import LoginPage from "./pages/LoginPage";
+import CreateAccountPage from "./pages/CreateAccountPage";
+import PerfilPage from "./pages/PerfilPage";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import CreateWorkout from "./pages/CreateWorkout";
+
 
 function App() {
   return (
     <div>
-      <PolygonTop />
-      <NavBar />
-      <Routes> /* En v6 usamos Routes en lugar de Route solo */
-      <Route index element={<HomePage />} />
+      <NavBar className="flex flex-col min-h-screen" />
+      <div className="flex-grow">
+      <Routes>
+        <Route index element={<HomePage />} />
         <Route path="/HomePage" element={<HomePage />} />
         <Route path="/Workouts" element={<WorkoutsPage />} />
         <Route path="/CsgStudio" element={<CsgPage />} />
+        <Route path="/CreateWorkout" element={<CreateWorkout />} />
         <Route path="/Login" element={<LoginPage />} />
         <Route element={<ProtectedRoute />} />
         <Route path="/CreateAccount" element={<CreateAccountPage />} />
         <Route path="/Perfil" element={<PerfilPage />} />
+        
       </Routes>
-      <PolygonBottom />
-      <FooterSection />
-
-
-    </div>
+      </div>
       
-    
+      <FooterSection />
+    </div>
   );
 }
 
