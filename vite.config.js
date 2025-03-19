232f53558c75785dd88d23ea62a69da1636797e7
Victor Lorencio <victorlorencio@gmail.com>
2025-03-19 17:03:05 +0100

    Reconfig viteConfigV1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',  // 👈 Asegúrate de que el base esté configurado así
+  
 });
