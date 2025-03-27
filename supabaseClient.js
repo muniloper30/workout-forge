1c5c2f283c1b986726006c65d8da729ff2ae8f98
Victor Lorencio <victorlorencio@gmail.com>
2025-03-27 16:01:24 +0100

    Fix comments

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,13 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Usar las variables de entorno definidas en el archivo .env
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; // URL de tu proyecto Supabase
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY; // Tu clave pública anon
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; // URL de la bd en Supabase
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY; // Clave
 
 // Crear la instancia de Supabase
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-console.log('Supabase URL:', supabaseUrl);  // Deberías ver la URL correcta
-console.log('Supabase Key:', supabaseKey);  // Deberías ver la clave correcta
+console.log('Supabase URL:', supabaseUrl);  // Comprobación URL correcta
+console.log('Supabase Key:', supabaseKey);  // Comprobación clave correcta
 
 export { supabase };
