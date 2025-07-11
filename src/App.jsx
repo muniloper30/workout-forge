435dc3fce6b5b54cded0095e0cc00e0a093edf8a
Victor Lorencio <victorlorencio@gmail.com>
2025-07-11 09:56:07 +0200

    Fix with lighthouse

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import { useEffect } from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage";
@@ -12,9 +17,7 @@ import PerfilPage from "./pages/PerfilPage";
 import ProtectedRoute from "./utils/ProtectedRoute";
 import CreateWorkout from "./pages/CreateWorkout";
 
-
 function App() {
-
   const location = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -24,20 +27,19 @@ function App() {
     <div>
       <NavBar className="flex flex-col min-h-screen" />
       <div className="flex-grow">
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/HomePage" element={<HomePage />} />
-        <Route path="/Workouts" element={<WorkoutsPage />} />
-        <Route path="/CsgStudio" element={<CsgPage />} />
-        <Route path="/CreateWorkout" element={<CreateWorkout />} />
-        <Route path="/Login" element={<LoginPage />} />
-        <Route element={<ProtectedRoute />} />
-        <Route path="/CreateAccount" element={<CreateAccountPage />} />
-        <Route path="/Perfil" element={<PerfilPage />} />
-        
-      </Routes>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/HomePage" element={<HomePage />} />
+          <Route path="/Workouts" element={<WorkoutsPage />} />
+          <Route path="/CsgStudio" element={<CsgPage />} />
+          <Route path="/CreateWorkout" element={<CreateWorkout />} />
+          <Route path="/Login" element={<LoginPage />} />
+          <Route element={<ProtectedRoute />} />
+          <Route path="/CreateAccount" element={<CreateAccountPage />} />
+          <Route path="/Perfil" element={<PerfilPage />} />
+        </Routes>
       </div>
-      
+
       <FooterSection />
     </div>
   );
