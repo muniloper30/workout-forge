d3a72e8fefbaa991684f9313aedad96a94ad0a0c
Victor Lorencio <victorlorencio@gmail.com>
2025-07-11 14:38:49 +0200

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import purgecss from 'vite-plugin-purgecss';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    purgecss({
-      content: [
-        './index.html',
-        './src/**/*.{js,jsx,ts,tsx,html}',
-      ],
-      // Puedes agregar una safelist si es necesario
-    }),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   base: "./",
 });
