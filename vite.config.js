9341dc6d747412eadc2cbcb5bb0bf140e33379d0
Victor Lorencio <victorlorencio@gmail.com>
2025-07-11 10:16:10 +0200

    purge css

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import purgecss from 'vite-plugin-purgecss';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    purgecss({
+      content: [
+        './index.html',
+        './src/**/*.{js,jsx,ts,tsx,html}',
+      ],
+      // Puedes agregar una safelist si es necesario
+    }),
+    tailwindcss(),
+  ],
   base: "./",
 });
